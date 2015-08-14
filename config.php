8053f2a9f2b61978ec5b0bbf440850f843ac4505
jchen <jchen@anchora.me>
2015-08-14 15:30:25 +0800

    ass

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/** MySQL数据库用户名USER */
+$db_username = '6825183e-ecb7';
+/** MySQL数据库密码PASSWORD */
+$db_password = 'bde88420-9173';
+/** 数据库的名称NAME */
+$db_name = 'd437f76633f1e4a46a784b13f95ca2356';
+/** MySQL主机HOSTNAME */
+$db_host = '192.168.1.13';
+?>
